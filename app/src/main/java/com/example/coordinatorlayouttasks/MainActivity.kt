2f25f898768964c6a63e6b02b925549c32a150ce
Gayane Aleksanyan <galexanyan@gmail.com>
2023-03-26 02:11:43 +0400

    adding Snackbar

--- app/src/main/java/com/example/coordinatorlayouttasks/MainActivity.kt
+++ app/src/main/java/com/example/coordinatorlayouttasks/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.coordinatorlayouttasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scroll_based)
+        setContentView(R.layout.activity_layout_based)
+
+        val click = findViewById<View>(R.id.fab)
+        val contextView = findViewById<View>(R.id.layout_based_activity)
+        val sb = Snackbar.make(contextView, "Hello!", Snackbar.LENGTH_SHORT)
+        click.setOnClickListener {
+            sb.show()
+        }
     }
+
 }
